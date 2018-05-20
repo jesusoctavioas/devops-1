74c777f7905a270c1416a29aa5f3438b7d213eae
jesusoctavioas <31106915+jesusoctavioas@users.noreply.github.com>
2018-05-20 09:01:43 -0500

    Correcion
    Limpieza de codigo

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello kevinpina.com\n");
+    response.end("PosArreconlaqueBarre.com\n");
 });
 
 // listen on localhost:8080
